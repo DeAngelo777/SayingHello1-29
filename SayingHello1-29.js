487eb8a240634cb968709dd55bd6eaeeb53e5004
Laimonas <ldirgelas530@gmail.com>
2024-08-01 12:41:06 +0300

    Initial commit

---
+++ SayingHello1-29.js
@@ -0,0 +1,12 @@
+// Output
+// What is your name? Laimonas
+// Hello, Laimonas, nice to meet you!
+
+let name = '';
+
+console.log(`What is your name? ${name}`);
+
+// Įrašomas savo vardas.
+name = 'Laimonas';
+
+console.log(`Hello, ${name}, nice to meet you!`);
